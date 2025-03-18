5932feaf7ae0257ed2eec306613c756c993728ea
Kennedy muchunguzi <kennedymuchunguzi@gmail.com>
2025-03-18 20:11:25 +0400

    adding a hero section video

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Navbar from "./components/Navbar";
+import HeroImage from './components/HeroImage';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <HeroImage />
     </div>
   );
 }
---
+++ src/components/HeroImage.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const HeroImage = () => {
+    return (
+        <div className="heroImage">
+              <h1>This is where real staff begins</h1>
+        </div>
+    )
+}
+
+export default HeroImage;
