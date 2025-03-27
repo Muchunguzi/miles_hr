3837fd2709ac22ad1e377ce35f5b5d5e6bf57033
Kennedy muchunguzi <kennedymuchunguzi@gmail.com>
2025-03-27 20:51:19 +0400

    added trusted companies

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Navbar from "./components/Navbar";
 import HeroImage from './components/HeroImage';
 import SearchJob from './components/SearchJob';
 import LatestJobs from './components/LatestJobs';
+import TrustedClients from './components/TrustedClients';
 
 function App() {
   
@@ -15,6 +16,7 @@ function App() {
       <HeroImage />
       <SearchJob Job = {Job}/>
       <LatestJobs />
+      <TrustedClients />
     </div>
   );
 }
